e0d5d4ccca3fa0a12254c0efc20d2083405aeae9
KodjoB <43495062+KodjoB@users.noreply.github.com>
2019-08-25 23:09:45 +0200

    Update program.sh

--- program.sh
+++ program.sh
@@ -5,8 +5,7 @@ mkdir h1programs 2>/dev/null
 mkdir ./h1programs/$foldername/ 2>/dev/null
 echo "[+]Scraping programs...."
 python geth1programs.py > ./h1programs/$foldername/program_list.txt
-nb_prog=wc -l ./h1programs/$foldername/program_list.txt
-echo "$nb_prog scraped."
+echo "$(wc -l ./h1programs/$foldername/program_list.txt) scraped."
 echo "[+]Scraping URLs...."
 for prog in $(cat ./h1programs/$foldername/program_list.txt)
 do
